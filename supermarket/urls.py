49e0db2ce40f27f658b8c1e44a468de328468728
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-18 15:55:33 -0300

    add files

--- supermarket/urls.py
+++ supermarket/urls.py
@@ -1,8 +1,11 @@
+from django import views
 from django.contrib import admin
 from django.urls import path
+from supermarket import views
+
 
 app_name = "market"
 
 urlpatterns = [
-    path("products/all")
+    path("products/all", views.home)
 ]
---
+++ supermarket/views/__init__.py
@@ -0,0 +1,1 @@
+from . super_market_views import *
