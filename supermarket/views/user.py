757d1c26a893071f74c735b916d1cd9f17d205f1
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-10-23 19:47:38 -0300

    add files

--- supermarket/views/user.py
+++ supermarket/views/user.py
@@ -24,10 +24,8 @@ def register_user(request):
 def login_user(request):
     if request.method == "GET":
         return render(request, "login.html")
-    
     username = request.POST.get("username") 
     password = request.POST.get("password")
-    
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
