05d822ffc9f374ecff5ee610b99fd31c5bfd59ef
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-25 21:37:11 -0300

    add files

--- supermarket/views/product.py
+++ supermarket/views/product.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from supermarket.models import Cart, InventoryItem, Product, CartItem, Inventory
 
 
-@login_required(login_url='market:login')
+# @login_required(login_url='market:login')
 def product(request, id):
     product = Product.objects.get(id=id)
     stock = product.stocks.first()
