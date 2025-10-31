a5d3ecbfa4fb8f4fdd3dd44465b7849ec6295b26
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-30 21:31:17 -0300

    add files

--- supermarket/views.py
+++ supermarket/views.py
@@ -4,6 +4,7 @@ from products.models import Products
 def home(request):
     user = request.user
     products = Products.objects.all()
+    print(products)
     return render(request ,"home.html",
         context={
             "products": products
