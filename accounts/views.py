3bcb54cb20eacb6b5cd1dd52ea5427ef0a21ad65
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-11-01 06:48:34 -0300

    add files

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
