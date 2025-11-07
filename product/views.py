5c583e756bc228dcc9946c05c8c9e10dc887c12f
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-11-06 22:05:50 -0300

    add files

--- product/views.py
+++ product/views.py
@@ -31,16 +31,20 @@ class BuyNowView(LoginRequiredMixin, View):
     #apply discount cupom
     def post(self, request, id):
         discount = request.POST.get("discount") or None
+        product = Product.objects.get(id=id)
+        stock = product.stocks.first()
         if discount:
             discount_search = DiscountCode.objects.filter(name=discount).first()
             if not discount_search:
+                previous_discount = request.session.get("discount_name")
+                previous_price = request.session.get("discount_price")
                 messages.error(request, "Cupom Inválido!")
-                return redirect('product:buynow', id)
-        product = Product.objects.get(id=id)
-        stock = product.stocks.first()
-        discount_price = product.price - (product.price / 100 * discount_search.discount)
+                return render(request, 'payment.html', {"product": product, "stock": stock, "discount_price": previous_price, "discount": previous_discount})
+        discount_price = float(product.price - (product.price / 100 * discount_search.discount))
+        request.session["discount_name"] = discount_search.name
+        request.session["discount_price"] = discount_price      
         messages.success(request, "Cupom de desconto aplicado com sucesso!!")
-        return render(request, 'payment.html', {"product": product, "stock": stock, "discount_price": discount_price})
+        return render(request, 'payment.html', {"product": product, "stock": stock, "discount_price": discount_price, "discount": discount})
 
 
 
