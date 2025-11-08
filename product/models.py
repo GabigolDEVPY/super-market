e88d8194d0481407e8cff3e09bf5b904f8588a70
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-11-08 15:53:51 -0300

    add files

--- product/models.py
+++ product/models.py
@@ -29,6 +29,7 @@ class Product(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image_full = models.ImageField(upload_to='products/', null=True, blank=True)
     
     def __str__(self):
         return self.name
@@ -51,6 +52,7 @@ class Stock(models.Model):
 class DiscountCode(models.Model):
     name = models.CharField(max_length=15, blank=False)
     discount = models.PositiveIntegerField()
+    
 
     def __str__(self):
         return self.name
