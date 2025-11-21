c87176f0f2b7afbd4a67225048a3a51be9a45ef9
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-11-21 18:20:34 -0300

    add files

--- payment/utils.py
+++ payment/utils.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 stripe.api_key = settings.API_STRIPE
 
-def create_checkout_session_product(user, metadata, items, product=None, price=None, quantity=None):
+def create_checkout_session_product(user, metadata, items):
     session = stripe.checkout.Session.create(
         payment_method_types=["card"],
         line_items=items,
