3b625282c6feffaf1d598137a54d946fefc747b2
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-12-12 21:15:10 -0300

    criando inlines

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . models import InfosForm, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(InfosForm)
+class InfosForm(admin.ModelAdmin):
+    list_display = ("user",) 
+    
+    
+    
+
+class OrderItem(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+    
+
+@admin.register(Order)
+class Order(admin.ModelAdmin):
+    inlines = [
+        OrderItem
+    ]
+    
+    list_display = ("pk",) 
--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,10 @@
+from email.policy import default
 from pyexpat import model
+from random import choice, choices
 from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from product.models import Product
 from stripe import Balance
 
 # Create your models here.
@@ -15,4 +18,25 @@ class InfosForm(models.Model):
     city = models.CharField(max_length=40, blank=False, null=False)
     cep = models.CharField(max_length=50, blank=False, null=False)
     state = models.CharField(max_length=50, blank=False, null=False)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    status = models.CharField(max_length=30 ,default="Pendente", choices=(
+        ('A', 'Aprovado'),
+        ('P', 'Pendente'),
+        ('R', 'Reprovado'),
+        ('E', 'Enviado'),
+        ('A', 'Finalizado')
+    ))
     
+    def __str__(self):
+        return f"Pedido N. {self.pk}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f"Item do pedido N. {self.order}"    
