a6c73e23da143496b6d878c3131910acc07c7460
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-12-27 14:23:45 -0300

    add files

--- supermarket/views.py
+++ supermarket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from product.models import Product, Category
+from django.db.models import Q
 from django.views.generic import ListView
 
 class HomeView(ListView):
@@ -19,8 +20,19 @@ class AllProducts(ListView):
     context_object_name = "products"
     paginate_by = 6
     ordering = ["id"]
-    
-    
+
+class SearchProduct(AllProducts):
+    def get_queryset(self, *args, **kwargs):
+        search_parameter = self.request.GET.get("parameter")
+        if not search_parameter:
+            return redirect("home")
+        query_set = super().get_queryset(*args, **kwargs)
+        query_set = query_set.filter(
+            Q(name__icontains=search_parameter) |
+            Q(category__icontains=search_parameter) |
+            Q(description__icontains=search_parameter) 
+            )
+        return query_set
 
 
     
