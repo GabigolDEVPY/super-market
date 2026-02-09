9af924871539c49824c7ae90f6b789a31221bd72
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2026-02-09 19:06:19 -0300

    arrumando arquivos finais

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -4,13 +4,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from .services import OrderCheckoutService
 
+
 @csrf_exempt
 def stripe_webhook(request):
     payload = request.body
     sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
     secret = settings.STRIPE_WEBHOOK_SECRET
     try:
-        event = stripe.Webhook.construct_event(payload, sig_header, secret)
+        event = stripe.Webhook.construct_event(payload, sig_header, settings.STRIPE_WEBHOOK_SECRET)
     except Exception:
         return HttpResponse(status=400)
     if event["type"] == "checkout.session.completed":
